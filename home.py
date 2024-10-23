2ba4131c2fc7e77e0e91dd4077ff2e2ba9c87654
Do Cong Huy <huydocong1502@gmail.com>
2024-10-23 16:27:38 +0700

    committed

--- home.py
+++ home.py
@@ -1,9 +1,30 @@
 import streamlit as st
 import os
+import base64
+
+@st.cache_data
+def get_img_as_base64(file):
+    with open(file, "rb") as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+def set_background():
+    img = get_img_as_base64("Home_background.png")
+    page_bg_img = f"""
+        <style>
+        [data-testid="stAppViewContainer"] > .main {{
+        background-image: url("data:image/png;base64,{img}");
+        background-size: 100%;
+        background-repeat: no-repeat;
+        background-attachment: local;
+        }}
+        </style>
+        """ 
+    st.markdown(page_bg_img, unsafe_allow_html=True)
 
 def display_home():
-    st.snow()
-
+    st.title("CALM |n_n|")
+    set_background()
     st.write("hello")
 
 display_home()
--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import streamlit as st
 import base64
 from st_pages import add_page_title, get_nav_from_toml
-    
+import webbrowser 
+from streamlit_option_menu import option_menu
+
 def main():
     nav = get_nav_from_toml("pages.toml")
-    pg = st.navigation(nav, expanded= False)
+    pg = st.navigation(nav)
     add_page_title(pg)
     pg.run()
-
-
     
 
 if __name__ == "__main__":
