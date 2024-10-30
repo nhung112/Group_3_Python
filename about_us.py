efc7ff93cfc30cb04077125121b48e1ef4d8d98c
nhung112 <nhung.312005@gmail.com>
2024-10-30 21:12:39 +0700

    committed

---
+++ about_us.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+def display_about_info():
+    st.title("About Us - CALM")
+    st.write("Have you ever stood in front of a kitchen with dozens of ingredients and didn't know what to make? Welcome to CALM, where you can find a variety of recipes. Each recipe is presented in detail, with illustrations and tips to help you easily make it even if it's your first time in the kitchen. In particular, with a friendly interface and convenient search tools, you can choose dishes according to ingredients and health status. Let's make every meal a relaxing experience and explore each dish every day with us!")
+
+st.header("Our Sloogan |n_n|")
+st.write('"Health - Nutrition - Happiness"')
+st.write("CALM's mission is to inspire people to experience healthy, nutritious and joyful meals. We always aim for recipes in the most optimal way to preserve nutritional value. Each dish provides all the necessary nutrients, ensuring a balance between flavor and health. More than just food, we hope you will find joy, relaxation and happiness when preparing and enjoying nutritious meals with your family.")
+
+st.header("Our Offerings:")
